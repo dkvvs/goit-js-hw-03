412bca9da2fbb7691a00b0a32efcdf548c8fccd9
Vitaliy Solomka <dkvvs@ukr.net>
2020-02-02 22:09:58 +0200

    add homework-03

---
+++ js/task-05.js
@@ -0,0 +1,26 @@
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function(arr, prop) {
+  const names = [];
+  for (const value of arr) {
+    if (prop in value) {
+      names.push(value[prop]);
+    }
+  }
+  return names;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+
+console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+
+console.log(getAllPropValues (products, 'category')); // []
